a62f2d916d3ba8e3873b1253d172887c73f6684f
unknown <tyagipriyanshi79@gmail.com>
2022-06-16 14:15:39 +0530

    Server

---
+++ Models/userModel.js
@@ -0,0 +1,42 @@
+import mongoose, { mongo } from "mongoose";
+
+const UserSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        firstname: {
+            type: String,
+            required: true
+        },
+
+        lastname : {
+            type: String,
+            required: true
+        },
+        isAdmin : {
+            type: Boolean,
+            required: false,
+        },
+
+        profilePicture: String,
+        coverPicture: String,
+        about: String,
+        livesin: String,
+        worksAt: String,
+        relationship: String,
+        followers: [],
+        following: []
+    },
+    {timestamps: true}
+)
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+//routes
+import AuthRoute from './Routes/AuthRoute.js';
+
+
+const app = express();
+//middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error)); // catching anytype of error if coming
+
+//usage of routes
+app.use('/auth', AuthRoute)
